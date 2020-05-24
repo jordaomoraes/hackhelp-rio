a99d91675335ef39c4359b21f9880cb726e0ce97
Guilherme Jordão <38707597+jordaomoraesl@users.noreply.github.com>
2020-05-24 12:57:17 -0300

    Mais Ajustes

--- config/auth.js
+++ config/auth.js
@@ -2,21 +2,21 @@ const localStrategy = require('passport-local').Strategy
 const mongoose =require('mongoose')
 const bcrypt = require('bcryptjs')
 
-// Model de usuário
+
     require('../models/Usuario')
     const Usuario = mongoose.model('usuarios')
 
-//Configurando estratégia do passport
-    module.exports = (passport) => {
-        passport.use(new localStrategy({usernameField: 'cpf', passwordField: 'senha'}, (cpf, senha, done) => {//Estratégia do passport E o campo de referencia para logar
 
-            Usuario.findOne({cpf: cpf}).then((usuario) =>{//procura 1 usuario com o cpf igual ao digitado
-                if(!usuario)//se for encontrado
+    module.exports = (passport) => {
+        passport.use(new localStrategy({usernameField: 'cpf', passwordField: 'senha'}, (cpf, senha, done) => {
+
+            Usuario.findOne({cpf: cpf}).then((usuario) =>{
+                if(!usuario)
                 {
                     return done(null, false, {message: 'Esta conta não existe!'});
                 }
 
-                bcrypt.compare(senha, usuario.senha, (erro, batem) => {//comparand a senha com a senha digitada
+                bcrypt.compare(senha, usuario.senha, (erro, batem) => {
                     if(batem)
                     {
                         return done(null, usuario)
@@ -29,11 +29,11 @@ const bcrypt = require('bcryptjs')
         }))
 
         passport.serializeUser((usuario, done) => {
-            done(null, usuario.id)//Manda os dados do usuario para uma sessão
+            done(null, usuario.id)
         })
 
         passport.deserializeUser((id, done) => {
-            Usuario.findById(id, (err, usuario) => {//procura o usuario pelo id
+            Usuario.findById(id, (err, usuario) => {
                 done(err, usuario)
             })
         })
