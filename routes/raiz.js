49c7b717e0994ae476cfa3267cd4705410b66166
Guilherme Jordão <38707597+jordaomoraesl@users.noreply.github.com>
2020-05-24 09:41:51 -0300

    Cadastros e funcionlidades Basicas!

---
+++ routes/raiz.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+//rota principal
+
+router.get('/',(req,res)=>{
+    res.render("index");
+})
+
+
+module.exports= router
